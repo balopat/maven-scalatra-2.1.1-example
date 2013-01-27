d91b93a4620341e20949b81ed822c74d600ee876
balopat <balopat@balopat.com>
2013-01-27 22:25:57 +0000

    initial import...

--- README.md
+++
@@ -1,4 +0,0 @@
-maven-scalatra-2.1.1-example
-============================
-
-maven-scalatra-2.1.1-example
---
+++ src/main/scala/com/example/MyScalatraWebApp.scala
@@ -0,0 +1,5 @@
+package com.example
+
+object MyScalatraWebApp extends App {
+  println("hello, world")
+}
